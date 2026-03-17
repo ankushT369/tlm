9f5660b93119bbf64f9f69423396d8041b502ae7
Ankush Mondal <mondalankush9851@gmail.com>
2026-03-17 05:47:21 +0000

    param

--- linenoise.h
+++ linenoise.h
@@ -99,7 +99,7 @@ int linenoiseHistoryAdd(const char *line);
 int linenoiseHistorySetMaxLen(int len);
 int linenoiseHistorySave(const char *filename);
 int linenoiseHistoryLoad(const char *filename);
-char** linenoiseHistoryGet();
+char** linenoiseHistoryGet(int *len);
 
 /* Other utilities. */
 void linenoiseClearScreen(void);
